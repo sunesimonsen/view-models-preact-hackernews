e0671702df3255c4fba32ef72b7164122e0cf2e1
Sune Simonsen <sune@we-knowhow.dk>
2026-03-23 14:33:16 +0100

    feat: scaffold project with Preact, Vite, and test config

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+
+export default defineConfig({
+  plugins: [preact()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
